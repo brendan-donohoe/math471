3cc229ed19df75f1a1ee3c2d139165357ceb9890
Brendan Donohoe <bddonohoe@unm.edu>
2016-09-21 10:38:46 -0600

    Updated gaussQuad code too.

--- Homework3/Code/gaussQuad.f90
+++ Homework3/Code/gaussQuad.f90
@@ -1,15 +1,15 @@
 program gaussQuad
 	implicit none
-	double precision :: f,Integral_value,x,pi
-	integer :: w,n,s
-	w = 1
+	double precision :: Integral_value,pi
+	double precision, dimension(:), allocatable :: x, f, w
+	integer :: n,s
 	pi = acos(dble(-1.d0))
     do s = 1,2
         do n = 2,1000
 
             allocate(x(0:n),f(0:n),w(0:n))
             call lglnodes(x,w,n)
-            f = exp(cos(x*(pi**dble(s)))
+            f = exp(cos(x*(pi**dble(s))))
             Integral_value = sum(f*w)
             deallocate(x,f,w)
         end do
